e3de54768f0d9c1df8c887db3242597b7583973a
eko abdul aziz <none>
2023-02-12 08:29:26 +0700

    create module say_hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/azizeko29/go-module-test
+
+go 1.19
---
+++ say_hello.go
@@ -0,0 +1,5 @@
+package go_module_test
+
+func SayHello() string {
+	return "Hello, selamat pagi. selamat beraktivitas ya :)"
+}
